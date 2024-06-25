9611ac5190f7e8da89d6c0050b7a0e20db801def
dashaun <dashaun@dashaun.com>
2024-06-25 16:34:48 -0500

    bash stats

--- demo.sh
+++ demo.sh
@@ -159,32 +159,7 @@ function statsSoFarTable {
 }
 
 function cfStats {
-  # Define the app names
-  apps=("springj8" "springj21" "springnative")
-  
-  while true; do
-      all_running=true
-      for app in "${apps[@]}"; do
-          status=$(cf app "$app" | grep '#0' | awk '{print $2}')
-          if [ "$status" != "running" ]; then
-              all_running=false
-              break
-          fi
-      done
-
-      if [ "$all_running" = true ]; then
-          break
-      fi
-      echo "Waiting for all apps to be running..."
-      sleep 5
-  done
-  
-  echo "--------------------------------------------------------------------------------------------"
-  echo "Same apps running on Cloud Foundry"
-  echo "--------------------------------------------------------------------------------------------"
-  cf app springj8 | grep "#0"
-  cf app springj21 | grep "#0"
-  cf app springnative | grep "#0"
+  bash "./stats.sh"
 }
 
 # Display Docker image statistics
