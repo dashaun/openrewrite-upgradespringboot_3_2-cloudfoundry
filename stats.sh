55843fea1a516a28cb986977842a0b47918ebf09
dashaun <dashaun@dashaun.com>
2024-06-25 16:23:54 -0500

    stats

--- stats.sh
+++ stats.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
-  echo "--------------------------------------------------------------------------------------------"
-  echo "Same apps running on Cloud Foundry"
-  echo "--------------------------------------------------------------------------------------------"
-  cf app springj8 | grep "#0"
-  cf app springj21 | grep "#0"
-  cf app springnative | grep "#0"
+while true; do
+    # Get the line containing '#0'
+    status1=$(cf app springj8 | grep '#0' | awk '{print $2}')
+    status2=$(cf app springj21 | grep '#0' | awk '{print $2}')
+    status3=$(cf app springnative | grep '#0' | awk '{print $2}')
+
+    
+    # Check if the status1 is 'running'
+    if [ "$status1" == "running" && "$status2" == "running" && "$status3" == "running" ]; 
+    then
+        break
+    fi
+    
+    # Wait for a few seconds before checking again
+    sleep 5
+done
+echo "--------------------------------------------------------------------------------------------"
+echo "Same apps running on Cloud Foundry"
+echo "--------------------------------------------------------------------------------------------"
+cf app springj8 | grep "#0"
+cf app springj21 | grep "#0"
+cf app springnative | grep "#0"
