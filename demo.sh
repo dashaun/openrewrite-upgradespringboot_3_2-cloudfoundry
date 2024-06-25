dc9a5660a55a989112fe595bbe0f58d8644c9606
dashaun <dashaun@dashaun.com>
2024-06-25 16:44:14 -0500

    cleanup

--- demo.sh
+++ demo.sh
@@ -3,10 +3,10 @@
 # Load helper functions and set initial variables
 vendir sync
 . ./vendir/demo-magic/demo-magic.sh
+
 export TYPE_SPEED=100
 export DEMO_PROMPT="${GREEN}➜ ${CYAN}\W ${COLOR_RESET}"
 JAVA_21="23.1.3.r21-nik"
-JAVA_17="17.0.11-librca"
 JAVA_8="8.0.412-librca"
 TEMP_DIR="upgrade-example"
 PROMPT_TIMEOUT=5
@@ -27,8 +27,8 @@ function initSDKman() {
     exit 1
   fi
   sdk update
-  sdk install java $JAVA_8
-  sdk install java $JAVA_21
+  sdk install java "$JAVA_8"
+  sdk install java "$JAVA_21"
 }
 
 # Prepare the working directory
@@ -39,30 +39,26 @@ function init {
   clear
 }
 
-# Switch to Java 8 and display version
-function useJava8 {
-  displayMessage "Use Java 8, this is for educational purposes only, don't do this at home! (I have jokes.)"
-  pei "sdk use java $JAVA_8"
-  pei "java -version" 
-}
-
-# Switch to Java 21 and display version
-function useJava21 {
-  displayMessage "Switch to Java 21 for Spring Boot 3"
-  pei "sdk use java $JAVA_21"
+# Switch to a specified Java version and display the version
+function useJava() {
+  local java_version=$1
+  local message=$2
+  displayMessage "$message"
+  pei "sdk use java $java_version"
   pei "java -version"
 }
 
-# Create a simple Spring Boot application
+# Clone a Spring Boot application
 function cloneApp {
   displayMessage "Clone a Spring Boot 2.6.0 application"
   pei "git clone https://github.com/dashaun/hello-spring-boot-2-6.git ./"
 }
 
-# Start the Spring Boot application
+# Start the Spring Boot application and log the output
 function springBootStart {
+  local log_file=$1
   displayMessage "Start the Spring Boot application"
-  pei "./mvnw -q clean package spring-boot:start -DskipTests 2>&1 | tee '$1' &"
+  pei "./mvnw -q clean package spring-boot:start -DskipTests 2>&1 | tee '$log_file' &"
 }
 
 # Stop the Spring Boot application
@@ -89,8 +85,10 @@ function showMemoryUsage {
 
 # Upgrade the application to Spring Boot 3.3
 function rewriteApplication {
-  displayMessage "Upgrade to Spring Boot 3.3 using a nested set of recipes from OpenRewrite"
-  pei "./mvnw -U org.openrewrite.maven:rewrite-maven-plugin:run -Drewrite.recipeArtifactCoordinates=org.openrewrite.recipe:rewrite-spring:LATEST -Drewrite.activeRecipes=org.openrewrite.java.spring.boot3.UpgradeSpringBoot_3_3"
+  displayMessage "Upgrade to Spring Boot 3.3 using OpenRewrite"
+  pei "./mvnw -U org.openrewrite.maven:rewrite-maven-plugin:run \
+      -Drewrite.recipeArtifactCoordinates=org.openrewrite.recipe:rewrite-spring:LATEST \
+      -Drewrite.activeRecipes=org.openrewrite.java.spring.boot3.UpgradeSpringBoot_3_3"
 }
 
 # Build a native image of the application
@@ -112,115 +110,105 @@ function stopNative {
   pei "kill -9 $npid"
 }
 
-# Build OCI images
-function buildOCI {
-  displayMessage "Build OCI images"
-  pei "docker pull dashaun/builder:tiny && docker tag dashaun/builder:tiny paketobuildpacks/builder:tiny && docker tag dashaun/builder:tiny paketobuildpacks/builder:base"
-  pei "./mvnw clean spring-boot:build-image -Dspring-boot.build-image.imageName=demo:0.0.1-JVM -Dspring-boot.build-image.createdDate=now"
-  pei "./mvnw clean -Pnative spring-boot:build-image -Dspring-boot.build-image.imageName=demo:0.0.1-Native -Dspring-boot.build-image.createdDate=now"
-}
-
 # Display a message with a header
 function displayMessage() {
   echo "#### $1"
   echo ""
 }
 
+# Extract startup time from log file
 function startupTime() {
-  echo "$(sed -nE 's/.* in ([0-9]+\.[0-9]+) seconds.*/\1/p' < $1)"
+  local log_file=$1
+  echo "$(sed -nE 's/.* in ([0-9]+\.[0-9]+) seconds.*/\1/p' < "$log_file")"
 }
 
+# Display a table comparing memory usage and startup times
 function statsSoFarTable {
   displayMessage "Comparison of memory usage and startup times"
   echo ""
-
+  
   # Headers
   printf "%-35s %-25s %-15s %s\n" "Configuration" "Startup Time (seconds)" "(MB) Used" "(MB) Savings"
   echo "--------------------------------------------------------------------------------------------"
 
   # Spring Boot 2.6 with Java 8
-  #STARTUP1=$(sed -nE 's/.* in ([0-9]+\.[0-9]+) seconds.*/\1/p' < java8with2.6.log)
-  #STARTUP1=$(grep -o 'Started HelloSpringApplication in .*' < java8with2.6.log)
   MEM1=$(cat java8with2.6.log2)
   printf "%-35s %-25s %-15s %s\n" "Spring Boot 2.6 with Java 8" "$(startupTime 'java8with2.6.log')" "$MEM1" "-"
 
   # Spring Boot 3.3 with Java 21
-  #STARTUP2=$(grep -o 'Started HelloSpringApplication in .*' < java21with3.3.log)
   MEM2=$(cat java21with3.3.log2)
   PERC2=$(bc <<< "scale=2; 100 - ${MEM2}/${MEM1}*100")
-  printf "%-35s %-25s %-15s %s \n" "Spring Boot 3.3 with Java 21" "$(startupTime 'java21with3.3.log')" "$MEM2" "$PERC2%"
+  printf "%-35s %-25s %-15s %s\n" "Spring Boot 3.3 with Java 21" "$(startupTime 'java21with3.3.log')" "$MEM2" "$PERC2%"
 
   # Spring Boot 3.3 with AOT processing, native image
-  #STARTUP3=$(grep -o 'Started HelloSpringApplication in .*' < nativeWith3.3.log)
   MEM3=$(cat nativeWith3.3.log2)
   PERC3=$(bc <<< "scale=2; 100 - ${MEM3}/${MEM1}*100")
-  printf "%-35s %-25s %-15s %s \n" "Spring Boot 3.3 with AOT, native" "$(startupTime 'nativeWith3.3.log')" "$MEM3" "$PERC3%"
-
+  printf "%-35s %-25s %-15s %s\n" "Spring Boot 3.3 with AOT, native" "$(startupTime 'nativeWith3.3.log')" "$MEM3" "$PERC3%"
 }
 
-function cfStats {
-  bash "./stats.sh"
-}
-
-# Display Docker image statistics
-function imageStats {
-  pei "docker images | grep demo"
-}
-
-# CF Push
+# Cloud Foundry push for non-native app
 function cfPush {
-  pei "cf push -f ../$1 > /dev/null  2>&1 &"
+  local manifest_file=$1
+  pei "cf push -f ../$manifest_file > /dev/null 2>&1 &"
 }
 
-# CF Push
+# Cloud Foundry push for native app
 function cfPushNative {
   pei "cd ../prebaked"
-  pei "cf push -f manifest-native.yml > /dev/null  2>&1 &"
-  pei "cd ../upgrade-example"
+  pei "cf push -f manifest-native.yml > /dev/null 2>&1 &"
+  pei "cd ../$TEMP_DIR"
 }
 
 # Main execution flow
 initSDKman
 init
-useJava8
+
+# Use Java 8 and perform operations
+useJava "$JAVA_8" "Use Java 8 for educational purposes"
 talkingPoint
 cloneApp
 talkingPoint
-springBootStart java8with2.6.log
+springBootStart "java8with2.6.log"
 talkingPoint
 validateApp
 talkingPoint
-showMemoryUsage "$(jps | grep 'HelloSpringApplication' | cut -d ' ' -f 1)" java8with2.6.log2
+showMemoryUsage "$(jps | grep 'HelloSpringApplication' | cut -d ' ' -f 1)" "java8with2.6.log2"
 talkingPoint
 springBootStop
 talkingPoint
-cfPush manifest-java8.yml
+cfPush "manifest-java8.yml"
 talkingPoint
+
+# Upgrade to Java 21 and perform operations
 rewriteApplication
 talkingPoint
-useJava21
+useJava "$JAVA_21" "Switch to Java 21 for Spring Boot 3"
 talkingPoint
-springBootStart java21with3.3.log
+springBootStart "java21with3.3.log"
 talkingPoint
 validateApp
 talkingPoint
-showMemoryUsage "$(jps | grep 'HelloSpringApplication' | cut -d ' ' -f 1)" java21with3.3.log2
+showMemoryUsage "$(jps | grep 'HelloSpringApplication' | cut -d ' ' -f 1)" "java21with3.3.log2"
 talkingPoint
 springBootStop
 talkingPoint
-cfPush manifest-java21.yml
+cfPush "manifest-java21.yml"
 talkingPoint
+
+# Build and run native image
 buildNative
 talkingPoint
 startNative
 talkingPoint
 validateApp
 talkingPoint
-showMemoryUsage "$(pgrep hello-spring)" nativeWith3.3.log2
+showMemoryUsage "$(pgrep hello-spring)" "nativeWith3.3.log2"
 talkingPoint
 stopNative
 talkingPoint
 cfPushNative
 talkingPoint
+
+# Display stats and images
 statsSoFarTable
 cfStats
